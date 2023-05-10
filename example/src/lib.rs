f4a4dd84f93b952d105816065e11ab524a1d870f
Rom's <romain.kelifa@gmail.com>
2023-05-10 17:51:09 +0700

    doc: improve example (#13)
    * Explicitly name example crate
    
    * Add reds definition to example
    
    * Document methods in example and how to test them
    
    and reformat for docs.rs
    
    * Add justfile for convenience
    
    * Update readme for justfile commands
    
    * Lint code
    
    * Add rusty hook
    
    * Remove unused workspace dep
    
    ---------
    
    Co-authored-by: jekky <11986158+jac3km4@users.noreply.github.com>

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -11,10 +11,25 @@ define_plugin! {
     }
 }
 
+/// call function with primitives
+///
+/// try in-game in CET console:
+///
+/// ```lua
+/// LogChannel(CName.new("DEBUG"), SumInts({ 2000, 77 }))
+/// ```
 fn sum_ints(ints: Vec<i32>) -> i32 {
     ints.iter().sum()
 }
 
+/// call function with game special types
+///
+/// try in-game in CET console:
+///
+/// ```lua
+/// UseTypes(CName.new("Test"), TDBID.Create("Items.BlackLaceV0"), ItemID.FromTDBID(TDBID.Create("Items.BlackLaceV0")), Game.GetPlayer():GetEntityID())
+/// ```
+/// > ⚠️ output can be found in mod's logs
 fn use_types(name: CName, tweak: TweakDbId, item: ItemId, entity: EntityId) {
     info!(
         "got CName {:#?}, TweakDBID {:#?}, ItemID {:#?}, EntityID {:#?}",
@@ -22,6 +37,14 @@ fn use_types(name: CName, tweak: TweakDbId, item: ItemId, entity: EntityId) {
     );
 }
 
+/// call function with handle
+///
+/// try in-game in CET console:
+///
+/// ```lua
+/// CallDemo(Game.GetPlayer())
+/// ```
+/// > ⚠️ output can be found in mod's logs
 fn call_demo(player: PlayerPuppet) {
     let res = add_u32(2, 2);
     info!("2 + 2 = {}", res);
@@ -31,29 +54,39 @@ fn call_demo(player: PlayerPuppet) {
     player.disable_camera_bobbing(true);
 }
 
-// import a global operator
-// function names gets automatically mangled
-// this one becomes OperatorAdd;Uint32Uint32;Uint32
+/// import a global operator
+///
+/// function names gets automatically mangled,
+/// this one becomes `OperatorAdd;Uint32Uint32;Uint32`
+///
+/// try in-game in CET console:
+///
+/// ```lua
+/// LogChannel(CName.new("DEBUG"), OperatorAdd(2000, 77))
+/// ```
+/// > ⚠️ output can be found in mod's logs
 #[redscript_global(name = "OperatorAdd", operator)]
 fn add_u32(l: u32, r: u32) -> u32;
 
-// define a binding for a class type
+/// define a binding for a class type
 #[derive(Clone, Default)]
 #[repr(transparent)]
 struct PlayerPuppet(Ref<IScriptable>);
 
 #[redscript_import]
 impl PlayerPuppet {
-    // imports 'public native func GetDisplayName() -> String'
-    // the method name is interpreted as PascalCase
-    // you can also specify it explicitly with a `name` attribute
+    /// imports `public native func GetDisplayName() -> String`
+    ///
+    /// the method name is interpreted as PascalCase
+    ///
+    /// you can also specify it explicitly with a `name` attribute
     #[redscript(native)]
     fn get_display_name(&self) -> String;
 
-    // imports 'private func GetUnlockedVehiclesSize() -> Int32'
+    /// imports `private func GetUnlockedVehiclesSize() -> Int32`
     fn get_unlocked_vehicles_size(&self) -> i32;
 
-    // imports 'private func DisableCameraBobbing(b: Bool) -> Void'
+    /// imports 'private func DisableCameraBobbing(b: Bool) -> Void'
     fn disable_camera_bobbing(&self, toggle: bool);
 }
 
