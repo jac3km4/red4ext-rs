f07127fde08eb6856e0230881579d5eebea55c81
jekky <11986158+jac3km4@users.noreply.github.com>
2023-05-12 01:42:56 +0100

    chore: change errors a bit

--- red4ext-sys/src/error.rs
+++ red4ext-sys/src/error.rs
@@ -7,10 +7,10 @@ pub enum ResourcePathError {
     #[error("resource path should not be empty")]
     Empty,
     #[error(
-        "resource path should be inferior to {} characters",
+        "resource path should be less than {} characters",
         ResourcePath::MAX_LENGTH
     )]
     TooLong,
-    #[error("resource path should be an absolute canonical path in archive e.g. 'base\\mod\\character.ent'")]
+    #[error("resource path should be an absolute canonical path in an archive e.g. 'base\\mod\\character.ent'")]
     NotCanonical,
 }
