2b4255ee39567fc167bb9221be43a41e286b4954
Roms1383 <romain.kelifa@gmail.com>
2023-12-15 09:10:13 +0700

    Prevent from using Display impl in unit-tests

--- red4ext-sys/src/interop.rs
+++ red4ext-sys/src/interop.rs
@@ -37,6 +37,7 @@ impl CName {
     }
 }
 
+#[cfg(not(test))] // only available in-game
 impl std::fmt::Display for CName {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", crate::ffi::resolve_cname(self))
