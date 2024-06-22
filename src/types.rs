f1edcd91a174f39858dabeefe06dfa428075eb99
Rom's <romain.kelifa@gmail.com>
2024-06-23 01:32:45 +0700

    feat: support RaRef and ResRef (#5)
    * feat: define resource path
    
    * feat: modularize and add res types
    
    * fix: no unwrap
    
    * fix: remove dead code annotations and expose constructors
    
    * fix: lint code
    
    * fix: missing re-exports
    
    * feat: define convenience macro
    
    * chore: remove resource path
    
    * fix: references
    
    * fix: macro
    
    also add tests and impls
    
    ---------
    
    Co-authored-by: jekky <11986158+jac3km4@users.noreply.github.com>

--- src/types.rs
+++ src/types.rs
@@ -2,5 +2,7 @@ mod entity_id;
 pub use entity_id::EntityId;
 mod item_id;
 pub use item_id::{GameEItemIdFlag, GamedataItemStructure, ItemId};
+mod res;
+pub use res::{RaRef, ResRef};
 mod tweak_db_id;
 pub use tweak_db_id::TweakDbId;
---
+++ src/types/res.rs
@@ -0,0 +1,209 @@
+use std::hash::Hash;
+use std::path::Path;
+
+use thiserror::Error;
+
+use crate::fnv1a64;
+use crate::raw::root::RED4ext as red;
+
+pub const MAX_LENGTH: usize = 216;
+
+#[derive(Debug, Default, Clone, Copy)]
+#[repr(transparent)]
+pub struct RaRef(red::RaRef);
+
+impl RaRef {
+    pub fn new(path: impl AsRef<Path>) -> Result<Self, ResourcePathError> {
+        Ok(Self(red::RaRef {
+            path: red::ResourcePath {
+                hash: encode_path(path)?,
+            },
+        }))
+    }
+}
+
+impl PartialEq for RaRef {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.path.hash.eq(&other.0.path.hash)
+    }
+}
+
+impl Eq for RaRef {}
+
+impl PartialOrd for RaRef {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for RaRef {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.path.hash.cmp(&other.0.path.hash)
+    }
+}
+
+impl Hash for RaRef {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.path.hash.hash(state);
+    }
+}
+
+#[derive(Debug, Default)]
+#[repr(transparent)]
+pub struct ResRef(red::ResRef);
+
+impl ResRef {
+    pub fn new(path: impl AsRef<Path>) -> Result<Self, ResourcePathError> {
+        Ok(Self(red::ResRef {
+            resource: red::RaRef {
+                path: red::ResourcePath {
+                    hash: encode_path(path)?,
+                },
+            },
+        }))
+    }
+}
+
+impl PartialEq for ResRef {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.resource.path.hash.eq(&other.0.resource.path.hash)
+    }
+}
+
+impl Eq for ResRef {}
+
+impl PartialOrd for ResRef {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for ResRef {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.resource.path.hash.cmp(&other.0.resource.path.hash)
+    }
+}
+
+impl Hash for ResRef {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.0.resource.path.hash.hash(state);
+    }
+}
+
+impl Clone for ResRef {
+    fn clone(&self) -> Self {
+        Self(red::ResRef {
+            resource: self.0.resource,
+        })
+    }
+}
+
+fn encode_path(path: impl AsRef<Path>) -> Result<u64, ResourcePathError> {
+    let sanitized = path
+        .as_ref()
+        .to_str()
+        .ok_or(ResourcePathError::InvalidUnicode)?;
+    let sanitized = sanitized
+        .trim_start_matches(['\'', '\"'])
+        .trim_end_matches(['\'', '\"'])
+        .trim_start_matches(['/', '\\'])
+        .trim_end_matches(['/', '\\'])
+        .split(['/', '\\'])
+        .filter(|comp| !comp.is_empty())
+        .map(str::to_ascii_lowercase)
+        .reduce(|mut acc, e| {
+            acc.push('\\');
+            acc.push_str(&e);
+            acc
+        })
+        .ok_or(ResourcePathError::Empty)?;
+    if sanitized.as_bytes().len() > self::MAX_LENGTH {
+        return Err(ResourcePathError::TooLong);
+    }
+    if Path::new(&sanitized)
+        .components()
+        .any(|x| !matches!(x, std::path::Component::Normal(_)))
+    {
+        return Err(ResourcePathError::NotCanonical);
+    }
+    Ok(fnv1a64(&sanitized))
+}
+
+#[derive(Debug, Error)]
+pub enum ResourcePathError {
+    #[error("resource path should not be empty")]
+    Empty,
+    #[error("resource path should be less than {} characters", self::MAX_LENGTH)]
+    TooLong,
+    #[error("resource path should be an absolute canonical path in an archive e.g. 'base\\mod\\character.ent'")]
+    NotCanonical,
+    #[error("resource path should be valid UTF-8")]
+    InvalidUnicode,
+}
+
+/// shortcut for ResRef creation.
+#[macro_export]
+macro_rules! res_ref {
+    ($base:expr, /$lit:literal $($tt:tt)*) => {
+        $crate::res_ref!($base.join($lit), $($tt)*)
+    };
+    ($base:expr, ) => {
+        $base
+    };
+    ($lit:literal $($tt:tt)*) => {
+        $crate::types::ResRef::new(
+            $crate::res_ref!(::std::path::Path::new($lit), $($tt)*)
+        )
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{encode_path, ResRef};
+    use crate::fnv1a64;
+
+    #[test]
+    fn resource_path() {
+        const TOO_LONG: &str = "base\\some\\archive\\path\\that\\is\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\very\\long\\and\\above\\216\\bytes";
+        assert!(TOO_LONG.as_bytes().len() > super::MAX_LENGTH);
+        assert!(encode_path(TOO_LONG).is_err());
+
+        assert_eq!(
+            encode_path("\'base/somewhere/in/archive/\'").unwrap(),
+            fnv1a64("base\\somewhere\\in\\archive")
+        );
+        assert_eq!(
+            encode_path("\"MULTI\\\\SOMEWHERE\\\\IN\\\\ARCHIVE\"").unwrap(),
+            fnv1a64("multi\\somewhere\\in\\archive")
+        );
+        assert!(encode_path("..\\somewhere\\in\\archive\\custom.ent").is_err());
+        assert!(encode_path("base\\somewhere\\in\\archive\\custom.ent").is_ok());
+        assert!(encode_path("custom.ent").is_ok());
+        assert!(encode_path(".custom.ent").is_ok());
+    }
+
+    #[test]
+    fn res_path() {
+        assert!(res_ref!("").is_err());
+        assert!(res_ref!(".." / "somewhere" / "in" / "archive" / "custom.ent").is_err());
+        assert!(res_ref!("base" / "somewhere" / "in" / "archive" / "custom.ent").is_ok());
+        assert!(res_ref!("custom.ent").is_ok());
+        assert!(res_ref!(".custom.ent").is_ok());
+
+        assert_eq!(
+            res_ref!("base" / "somewhere" / "in" / "archive" / "custom.ent").unwrap(),
+            ResRef::new(std::path::Path::new(
+                "base\\somewhere\\in\\archive\\custom.ent"
+            ))
+            .unwrap()
+        );
+        assert_eq!(
+            res_ref!("custom.ent").unwrap(),
+            ResRef::new(std::path::Path::new("custom.ent")).unwrap()
+        );
+        assert_eq!(
+            res_ref!(".custom.ent").unwrap(),
+            ResRef::new(std::path::Path::new(".custom.ent")).unwrap()
+        );
+    }
+}
