8679c05b97252dfba8e565a9430097e3ee84e192
Roms1383 <romain.kelifa@gmail.com>
2024-07-17 08:12:51 +0700

    refactor: more derives

--- src/types/opt.rs
+++ src/types/opt.rs
@@ -5,7 +5,7 @@ use std::fmt;
 ///
 /// When left unspecified on Redscript side,
 /// it translates to its `Default` representation.
-#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 pub enum Opt<T> {
     /// Value is specified and guaranteed to be non-`Default` value.
     NonDefault(T),
@@ -56,32 +56,6 @@ where
             Self::NonDefault(x) if x == other => true,
             Self::Default if other == &T::default() => true,
             _ => false,
-        }
-    }
-}
-
-impl<T> Ord for Opt<T>
-where
-    T: Default + Ord,
-{
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        match (self, other) {
-            (Self::NonDefault(lhs), Self::NonDefault(rhs)) => lhs.cmp(rhs),
-            (Self::NonDefault(lhs), Self::Default) => lhs.cmp(&T::default()),
-            (Self::Default, Self::NonDefault(rhs)) => T::default().cmp(rhs),
-            (Self::Default, Self::Default) => std::cmp::Ordering::Equal,
-        }
-    }
-}
-
-impl<T> PartialOrd<T> for Opt<T>
-where
-    T: Default + PartialOrd + Ord,
-{
-    fn partial_cmp(&self, other: &T) -> Option<std::cmp::Ordering> {
-        match (self, other) {
-            (Self::NonDefault(lhs), rhs) => Some(lhs.cmp(rhs)),
-            (Self::Default, rhs) => Some(T::default().cmp(rhs)),
         }
     }
 }
