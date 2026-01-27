629353ee8ee5b9c961deef82e03d3f5e04640023
Rom's <romain.kelifa@gmail.com>
2026-01-28 00:18:33 +0700

    Plugin StateHandler return type (#84)
    * chore: ignore .zed
    
    * chore: ignore .vs
    
    * chore: plugin ops on exit
    
    * chore: format
    
    * chore: lint
    
    * fix: matching wildcard
    
    * fix: state listener handler result
    
    * docs: typo
    
    * chore: main return
    
    * docs: typo
    
    * docs: typo
    
    * docs: typo

--- src/lib.rs
+++ src/lib.rs
@@ -83,6 +83,18 @@ pub trait Plugin {
 
     /// A function that is called when the plugin is initialized.
     fn on_init(_env: &SdkEnv) {}
+    fn on_exit(_env: &SdkEnv) {}
+}
+
+/// [StateHandler] execution result,
+/// which can be executed across multiple frames.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[repr(u8)]
+pub enum StateHandlerResult {
+    /// [StateHandler] hasn't finished executing (and will be called again next frame).
+    Running = 0,
+    /// [StateHandler] has finished executing (and lifecycle can carry on to next step).
+    Finished = 1,
 }
 
 /// A set of useful operations that can be performed on a plugin.
@@ -98,6 +110,8 @@ pub trait PluginOps: Plugin {
     fn info() -> PluginInfo;
     #[doc(hidden)]
     fn init(env: SdkEnv);
+    #[doc(hidden)]
+    fn exit(env: SdkEnv);
 }
 
 #[sealed]
@@ -139,6 +153,10 @@ where
 
         Self::on_init(Self::env());
     }
+
+    fn exit(env: SdkEnv) {
+        Self::on_exit(&env);
+    }
 }
 
 /// Defines a set of DLL symbols necessary for RED4ext to load the plugin. Your plugin will
@@ -163,11 +181,18 @@ macro_rules! export_plugin_symbols {
                 handle: $crate::internal::PluginHandle,
                 reason: $crate::internal::EMainReason::Type,
                 sdk: $crate::internal::Sdk,
-            ) {
-                if reason == $crate::internal::EMainReason::Load {
-                    <$trait as $crate::PluginOps>::init($crate::SdkEnv::new(handle, sdk));
-                    $crate::RttiRegistrator::add(Some(on_register), Some(on_post_register));
+            ) -> bool {
+                match reason {
+                    $crate::internal::EMainReason::Load => {
+                        <$trait as $crate::PluginOps>::init($crate::SdkEnv::new(handle, sdk));
+                        $crate::RttiRegistrator::add(Some(on_register), Some(on_post_register));
+                    }
+                    $crate::internal::EMainReason::Unload => {
+                        <$trait as $crate::PluginOps>::exit($crate::SdkEnv::new(handle, sdk));
+                    }
+                    _ => {}
                 }
+                true
             }
 
             #[unsafe(no_mangle)]
@@ -364,7 +389,7 @@ impl SdkEnv {
     ///
     /// # Example
     /// ```rust
-    /// use red4ext_rs::{GameApp, SdkEnv, StateListener, StateType};
+    /// use red4ext_rs::{GameApp, SdkEnv, StateHandlerResult, StateListener, StateType};
     ///
     /// fn add_state_listener(env: &SdkEnv) {
     ///     let listener = StateListener::default()
@@ -373,12 +398,14 @@ impl SdkEnv {
     ///     env.add_listener(StateType::Running, listener);
     /// }
     ///
-    /// unsafe extern "C" fn on_enter(app: &GameApp) {
+    /// unsafe extern "C" fn on_enter(app: &GameApp) -> StateHandlerResult {
     ///     // do something here...
+    ///     StateHandlerResult::Finished
     /// }
     ///
-    /// unsafe extern "C" fn on_exit(app: &GameApp) {
+    /// unsafe extern "C" fn on_exit(app: &GameApp) -> StateHandlerResult {
     ///     // do something here...
+    ///     StateHandlerResult::Finished
     /// }
     /// ```
     #[inline]
@@ -643,7 +670,7 @@ impl_fn_ptr!(A, B, C, D, E, F, G, H, I);
 impl_fn_ptr!(A, B, C, D, E, F, G, H, I, J);
 
 /// A callback function to be called when a state is entered, updated, or exited.
-pub type StateHandler = unsafe extern "C" fn(app: &GameApp);
+pub type StateHandler = unsafe extern "C" fn(app: &GameApp) -> StateHandlerResult;
 
 /// A wrapper around the game application instance.
 #[repr(transparent)]
