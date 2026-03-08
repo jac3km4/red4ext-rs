5c02154892dfaacacd069ab5e7a4293980fc37ac
Rom's <romain.kelifa@gmail.com>
2026-03-08 13:30:20 +0700

    Allow support for native game systems (#87)
    * chore(vft): ISerializable
    
    * chore(vft): IScriptable
    
    * feat: IUpdatableSystem
    
    * feat: IGameSystem
    
    * chore: expose types and vfts
    
    * chore: lint

--- src/types/game_engine.rs
+++ src/types/game_engine.rs
@@ -4,6 +4,7 @@ use super::{IScriptable, Ref, Type};
 use crate::class::{ScriptClass, class_kind};
 use crate::raw::root::RED4ext as red;
 use crate::types::WeakRef;
+use crate::types::rtti::IScriptableVft;
 use crate::{NativeRepr, VoidPtr};
 
 /// Scripted game instance.
@@ -105,4 +106,82 @@ impl AsRef<IScriptable> for ScriptableSystem {
     fn as_ref(&self) -> &IScriptable {
         unsafe { mem::transmute(&self.0._base._base) }
     }
+}
+
+#[repr(transparent)]
+pub struct IUpdatableSystem(IScriptable);
+
+unsafe impl ScriptClass for IUpdatableSystem {
+    type Kind = class_kind::Native;
+
+    const NAME: &'static str = "IUpdatableSystem";
+}
+
+impl IUpdatableSystem {
+    #[inline]
+    pub fn vft(&self) -> &IUpdatableSystemVft {
+        unsafe { mem::transmute::<&IScriptableVft, &IUpdatableSystemVft>(self.0.vft()) }
+    }
+}
+
+impl AsRef<IScriptable> for IUpdatableSystem {
+    fn as_ref(&self) -> &IScriptable {
+        unsafe { mem::transmute(&self.0) }
+    }
+}
+
+#[repr(C)]
+pub struct IGameSystem {
+    base: IUpdatableSystem,
+    game_instance: *mut NativeGameInstance,
+}
+
+unsafe impl ScriptClass for IGameSystem {
+    type Kind = class_kind::Native;
+
+    const NAME: &'static str = "gameIGameSystem";
+}
+
+impl IGameSystem {
+    #[inline]
+    pub fn vft(&self) -> &IGameSystemVft {
+        unsafe { mem::transmute::<&IUpdatableSystemVft, &IGameSystemVft>(self.base.vft()) }
+    }
+}
+
+impl AsRef<IScriptable> for IGameSystem {
+    fn as_ref(&self) -> &IScriptable {
+        unsafe { mem::transmute(&self.base.0) }
+    }
+}
+
+#[repr(C)]
+pub struct IUpdatableSystemVft {
+    base: IScriptableVft,
+    on_register_updates: unsafe extern "C" fn(this: VoidPtr, registrar: VoidPtr),
+}
+
+#[repr(C)]
+pub struct IGameSystemVft {
+    base: IUpdatableSystemVft,
+    on_world_attached: VoidPtr,
+    on_before_world_detach: VoidPtr,
+    on_world_detached: VoidPtr,
+    on_after_world_detach: VoidPtr,
+    on_before_game_save: VoidPtr,
+    on_game_save: VoidPtr,
+    on_after_game_save: VoidPtr,
+    on_game_load: VoidPtr,
+    on_game_restored: VoidPtr,
+    on_game_prepared: VoidPtr,
+    on_game_paused: VoidPtr,
+    on_game_resumed: VoidPtr,
+    is_saving_locked: VoidPtr,
+    on_streaming_world_loaded: VoidPtr,
+    sub_180: VoidPtr,
+    sub_188: VoidPtr,
+    sub_190: VoidPtr,
+    sub_198: VoidPtr,
+    on_initialize: VoidPtr,
+    on_uninitialize: VoidPtr,
 }
