e992afae428d34d0072559ddf38244e215693cc5
Roms1383 <romain.kelifa@gmail.com>
2026-04-15 18:34:32 +0700

    perf: avoid allocation

--- src/types/l12n.rs
+++ src/types/l12n.rs
@@ -1,10 +1,16 @@
-use crate::{raw::root::RED4ext as red, types::RedString};
+use crate::raw::root::RED4ext as red;
 
 #[repr(transparent)]
 pub struct LocalizationString(red::LocalizationString);
 
 impl std::fmt::Display for LocalizationString {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", RedString::from(&self.0.unk08))
+        write!(
+            f,
+            "{}",
+            unsafe { std::ffi::CStr::from_ptr(self.0.unk08.c_str()) }
+                .to_str()
+                .unwrap_or_default()
+        )
     }
 }
